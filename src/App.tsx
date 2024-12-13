c6a64787c1ac9f2a735827196656aaf6df004be1
emapg <emap04@proton.me>
2024-12-13 20:07:21 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Suspense } from 'react';
 import Hero from './components/sections/Hero';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import About from './components/sections/About';
+import Contact from './components/sections/Contact';
+import Projects from './components/sections/Projects';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Hero />
       </Suspense>
+      <About />
+      <Projects />
+      <Contact />
       <Footer />
     </div>
   );
